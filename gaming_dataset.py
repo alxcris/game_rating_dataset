b5a57bb8161fdbd0a807c487545a50b48db83d94
alxcris <ilie_alexandru26@yahoo.com>
2026-05-24 23:19:09 +0300

    did EDA, data cleaning, train/test csv

--- gaming_dataset.py
+++ gaming_dataset.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
 import os
+
+from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
 
 #ca sa nu dam reveal la api_key(ca sa il pun pe github)
@@ -26,10 +28,10 @@ for pagina in range(1, 26):
         for joc in date_dictionar['results']:
             
             #verificam daca jocul are o nota de la metacritic
-            if(joc.get('metacritic') != 0):
+            if(joc.get('metacritic') is not None):
                 
-                # Extragem genul cu if/else normal ca să fie ușor de înțeles
-                if len(joc.get('genres', [])) > 0:
+                #vedem ce gen e jocul
+                if(len(joc.get('genres', [])) > 0):
                     gen = joc['genres'][0]['name']
                 else:
                     gen = "Unknown"
@@ -48,13 +50,15 @@ for pagina in range(1, 26):
                     'nr_platforme': len(joc.get('platforms', [])),
                     'timp_joc_mediu': joc.get('playtime', 0),
                     'rating_utilizatori': joc.get('rating', 0),
+                    'nr_recenzii': joc.get('reviews_count', 0),
+                    'nr_realizari': joc.get('achievements_count', 0),
                     'metacritic': joc['metacritic']
                 }
                 
                 #salvam hashtableul in lista de jocuri
                 toate_jocurile.append(joc_curent)
                 
-        print(f"Am terminat de citit pagina {pagina}.")
+        print(f"pagina {pagina}.")
 
 #facem lista de hashtable intr-un dataframe
 df = pd.DataFrame(toate_jocurile)
@@ -62,10 +66,18 @@ df = pd.DataFrame(toate_jocurile)
 #daca scorul metacritic e mai mare ca 80,salvam 1 pentru hit,altfel 0
 df['este_hit'] = (df['metacritic'] >= 80).astype(int)
 
-# Ștergem coloana metacritic initiala ca trainingul sa nu fie biased
-df = df.drop(columns=['metacritic'], axis=1) 
+#adaugam df.drop pe metacritic ca modelul sa nu dea train doar pe metacritic
+df = df.drop(columns=['metacritic'])
 
-#facem un fisier csv pentru tabel
-df.to_csv("dataset_jocuri.csv", index=False)
+#impartim dataset-ul pentru tin si test
+df_train, df_test = train_test_split(df, test_size=0.3, random_state=42)
 
-print(f"\nAu fost salvate {len(df)} jocuri cu succes.")
+#afisam numarul de date de train si de test pentru verificare
+print(f"\nSet antrenare: {len(df_train)} instante.")
+print(f"Set testare: {len(df_test)} instante.")
+
+#punem dataset-ul de training si cel de test in fisiere diferite
+df_train.to_csv("train_dataset.csv", index=False)
+df_test.to_csv("test_dataset.csv", index=False)
+
+print("Fisierele CSV s au format cu succes")
